89c746628d20ceea24b9dd6a9a84f5badae29010
SimonWoodtli <xnasero@posteo.net>
2023-03-15 13:47:20 -0500

    🐛 fix script to get correct filepath
    If script was run outside of repo it would fail because normally shell
    uses $PWD and executes cmds from there, the chezmoi.yaml.tmpl could be
    only initialized if `chezmoi -S custompath init` was run within the
    repo. But not outside. This addition will fetch the filepath of the
    script itself and use that for the git command.

--- template-scripts/get-github-username.sh
+++ template-scripts/get-github-username.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-remoteUrl=$(chezmoi git -- config --get remote.origin.url)
+#remoteUrl=$(chezmoi git -- config --get remote.origin.url)
+filepath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+remoteUrl=$(git -C "$filepath" config --get remote.origin.url)
 # filter username from url
 githubUsername=${remoteUrl%/*}
 githubUsername=${githubUsername##*:}
