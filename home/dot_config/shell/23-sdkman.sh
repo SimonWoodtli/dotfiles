1f8262c0049718a10e7618b5f13367567489831f
SimonWoodtli <xnasero@posteo.net>
2023-03-16 17:42:36 -0500

    🚚 source sdkman from ~/.config/shell/

---
+++ home/dot_config/shell/23-sdkman.sh
@@ -0,0 +1,5 @@
+
+################################ SDKMAN ################################
+
+#This needs to be at the end of your bashrc to work
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
