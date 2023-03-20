97ec15208b408b53f580b15260c66f0937f44211
SimonWoodtli <xnasero@posteo.net>
2023-03-20 10:07:09 -0500

    🔧 add cry to autocompletion

--- home/dot_config/shell/11-completion.sh
+++ home/dot_config/shell/11-completion.sh
@@ -7,7 +7,7 @@ if [ -r /usr/share/bash-completion/bash_completion ]; then
   source /usr/share/bash-completion/bash_completion
 fi
 
-owncomp=( pdf yt zet sshkey gm lorem tidder snippet )
+owncomp=( cry pdf yt zet sshkey gm lorem tidder snippet )
 for i in "${owncomp[@]}"; do complete -C "$i" "$i"; done
 
 ## autocomplete for tldr
