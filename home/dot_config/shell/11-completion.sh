fb4d963393f41c89ff2345e382007f955529348e
SimonWoodtli <xnasero@posteo.net>
2023-05-16 13:26:43 -0500

    🔧 add nix bash completion

--- home/dot_config/shell/11-completion.sh
+++ home/dot_config/shell/11-completion.sh
@@ -23,3 +23,22 @@ _have pandoc && . <(pandoc --bash-completion)
 _have docker && _source_if "$HOME/.local/share/docker/completion" # d
 _have docker-compose && complete -F _docker_compose dc # dc
 _have deno && source /usr/local/etc/bash_completion.d/deno.bash
+
+# Check whether we're running a version of Bash that has support for
+# programmable completion. If we do, enable all modules installed in
+# the system and user profile in obsolete /etc/bash_completion.d/
+# directories. Bash loads completions in all
+# $XDG_DATA_DIRS/bash-completion/completions/
+# on demand, so they do not need to be sourced here.
+if _have nix && shopt -q progcomp &>/dev/null; then
+  . "/nix/store/bcjj9j8xqbwqx4fcsxydya671pgl5nzq-bash-completion-2.11/etc/profile.d/bash_completion.sh"
+  nullglobStatus=$(shopt -p nullglob)
+  shopt -s nullglob
+  for p in $NIX_PROFILES; do
+    for m in "$p/etc/bash_completion.d/"*; do
+      . "$m"
+    done
+  done
+  eval "$nullglobStatus"
+  unset nullglobStatus p m
+fi
