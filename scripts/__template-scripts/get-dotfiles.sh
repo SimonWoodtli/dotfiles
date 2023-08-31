964216295a32b0cff52a70012923a9ced6a41938
SimonWoodtli <xnasero@posteo.net>
2023-08-31 13:46:56 -0500

    🔧 add template script get-dotfiles.sh

---
+++ scripts/__template-scripts/get-dotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ ! -d "$HOME/Repos/github.com/SimonWoodtli" ]]; then                       
+  folder="$HOME/Repos/github.com/SimonWoodtli/"                                 
+  git -C "$folder" clone https://github.com/SimonWoodtli/dotfiles.git           
+  git -C "$folder" clone https://github.com/SimonWoodtli/zet.git                
+fi
