3640d8329ef037a02a4b38939119f631021b63b4
SimonWoodtli <xnasero@posteo.net>
2023-09-02 12:33:35 -0500

    🩹 add mkdir cmd

--- scripts/__template-scripts/get-dotfiles.sh
+++ scripts/__template-scripts/get-dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ ! -d "$HOME/Repos/github.com/SimonWoodtli" ]]; then                       
   folder="$HOME/Repos/github.com/SimonWoodtli/"                                 
+  mkdir -p "$folder"
   git -C "$folder" clone https://github.com/SimonWoodtli/dotfiles.git           
   git -C "$folder" clone https://github.com/SimonWoodtli/zet.git                
 fi
