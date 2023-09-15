12b7efe0ec55ebf972a6da9174e3bda12bed0f05
SimonWoodtli <xnasero@posteo.net>
2023-09-14 20:41:47 -0500

    ✨ add wastemem.c

---
+++ scripts/wastemem.c
@@ -0,0 +1,57 @@
+/* simple program to defragment memory, J. Cooperstein 2/04-1/2020
+1. compile with `gcc -o wastemem wastemem.c`
+2. waste 4GB of ram: ./wastemem 4096*/
+
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <signal.h>
+ 
+#define MB (1024*1024)
+#define BS 16           /* will allocate BS*MB at each step */
+#define CHUNK (MB*BS)
+#define QUIT_TIME 20
+void quit_on_timeout(int sig)
+{
+    printf("\n\nTime expired, quitting\n");
+    exit(EXIT_SUCCESS);
+}   
+ 
+int main(int argc, char **argv)
+{
+    struct sysinfo si;
+    int j, m;
+    char *c;
+ 
+    /* get total memory on the system */
+    sysinfo(&si);
+    m = si.totalram / MB;
+    printf("Total System Memory in MB = %d MB\n", m);
+    m = (9 * m) / 10;   /* drop 10 percent */
+    printf("Using somewhat less: %d MB\n", m);
+ 
+    if (argc == 2) {
+        m = atoi(argv[1]);
+        printf("Choosing instead mem = %d MB\n", m);
+    }
+ 
+    signal(SIGALRM, quit_on_timeout);
+    printf("Will quite in QUIT_TIME seconds if no normal termination\n");
+    alarm(QUIT_TIME);
+ 
+    for (j = 0; j <= m; j += BS) {
+        /* yes we know this is a memory leak, no free,
+         * that's the idea!
+         */
+        c = malloc(CHUNK);
+        /* just fill the block with j over and over */
+        memset(c, j, CHUNK);
+        printf("%8d", j);
+        fflush(stdout);
+    }
+    printf("\n\n    Quitting and releasing memory\n");
+    exit(EXIT_SUCCESS);
+}
