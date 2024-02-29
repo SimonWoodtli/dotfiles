14f56e0fdb437e2512c6e766a67e3c6197389828
SimonWoodtli <me@simonwoodtli.com>
2024-02-29 11:44:33 -0500

    🔧 use hash for has by default

--- home/dot_config/shell/02-utility-functions.sh
+++ home/dot_config/shell/02-utility-functions.sh
@@ -7,3 +7,10 @@ onmac() { [ "$PLATFORM" == mac ] && return 0 || return 1; }
 onwin() { [ "$PLATFORM" == windows ]  && return 0 || return 1; }
 onlinux() { [ "$PLATFORM" == linux ]  && return 0 || return 1; }
 onunknown() { [ "$PLATFORM" == unknown ]  && return 0 || return 1; }
+
+if hash &>/dev/null; then
+  _have() { hash "$1" &>/dev/null; }
+else
+	# hashing disabled (NixOS)
+	has() { type "$1" &>/dev/null; }
+fi
